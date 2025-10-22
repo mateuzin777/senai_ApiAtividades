e74c1f0ca2f1cc52c17b2b5defc241b1b3b7716b
Mateus Gonçalves <mateus270516@gmail.com>
2025-10-22 20:16:48 -0300

    Enviar projeto de API do SENAI

--- Controllers/Usuarios.Controller.cs
+++ Controllers/Usuarios.Controller.cs
@@ -3,26 +3,43 @@ using Exo.WebApi.Repositories;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
 namespace Exo.WebApi.Controllers
 {
-[Produces("application/json")]
-[Route("api/[controller]")]
-[ApiController]
-public class UsuariosController : ControllerBase
-{
-private readonly UsuarioRepository _usuarioRepository;
-public UsuariosController(UsuarioRepository
-usuarioRepository)
-{
-_usuarioRepository = usuarioRepository;
-}
-// get -> /api/usuarios
-[HttpGet]
-public IActionResult Listar()
-{
-return Ok(_usuarioRepository.Listar());
-}
-        // post -> /api/usuarios
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuariosController : ControllerBase
+    {
+        private readonly UsuarioRepository _usuarioRepository;
+
+        public UsuariosController(UsuarioRepository usuarioRepository)
+        {
+            _usuarioRepository = usuarioRepository;
+        }
+
+        // GET -> /api/usuarios
+        [HttpGet]
+        public IActionResult Listar()
+        {
+            return Ok(_usuarioRepository.Listar());
+        }
+
+        // GET -> /api/usuarios/{id}
+        [HttpGet("{id}")]
+        public IActionResult BuscarPorId(int id)
+        {
+            Usuario usuario = _usuarioRepository.BuscaPorId(id);
+            if (usuario == null)
+                return NotFound();
+
+            return Ok(usuario);
+        }
+
+        // POST -> /api/usuarios
         [HttpPost]
         public IActionResult Cadastrar(Usuario usuario)
         {
@@ -30,38 +47,59 @@ return Ok(_usuarioRepository.Listar());
             return StatusCode(201);
         }
 
-// get -> /api/usuarios/{id}
-[HttpGet("{id}")] // Faz a busca pelo ID.
-public IActionResult BuscarPorId(int id)
-{
-Usuario usuario = _usuarioRepository.BuscaPorId(id);
-if (usuario == null)
-{
-return NotFound();
-}
-return Ok(usuario);
-}
-// put -> /api/usuarios/{id}
-// Atualiza.
-[HttpPut("{id}")]
-public IActionResult Atualizar(int id, Usuario usuario)
-{
-_usuarioRepository.Atualizar(id, usuario);
-return StatusCode(204);
-}
-// delete -> /api/usuarios/{id}
-[HttpDelete("{id}")]
-public IActionResult Deletar(int id)
-{
-try
-{
-_usuarioRepository.Deletar(id);
-return StatusCode(204);
-}
-catch (Exception e)
-{
-return BadRequest();
-}
-}
-}
+        // POST -> /api/usuarios/login
+        [AllowAnonymous]
+        [HttpPost("login")]
+        public IActionResult Login(Usuario usuario)
+        {
+            Usuario usuarioBuscado = _usuarioRepository.Login(usuario.Email, usuario.Senha);
+            if (usuarioBuscado == null)
+                return NotFound("E-mail ou senha inválidos!");
+
+            // Cria o payload do token
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Email, usuarioBuscado.Email),
+                new Claim(JwtRegisteredClaimNames.Jti, usuarioBuscado.Id.ToString()),
+            };
+
+            var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("exoapi-chave-autenticacao"));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: "exoapi.webapi",
+                audience: "exoapi.webapi",
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(30),
+                signingCredentials: creds
+            );
+
+            return Ok(new { token = new JwtSecurityTokenHandler().WriteToken(token) });
+        }
+
+        // PUT -> /api/usuarios/{id}
+        [Authorize]
+        [HttpPut("{id}")]
+        public IActionResult Atualizar(int id, Usuario usuario)
+        {
+            _usuarioRepository.Atualizar(id, usuario);
+            return StatusCode(204);
+        }
+
+        // DELETE -> /api/usuarios/{id}
+        [Authorize]
+        [HttpDelete("{id}")]
+        public IActionResult Deletar(int id)
+        {
+            try
+            {
+                _usuarioRepository.Deletar(id);
+                return StatusCode(204);
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+    }
 }
